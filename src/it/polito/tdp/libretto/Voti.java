15cd6ffea04f0ebc910163fac40f98035f5bd365
Marina <Marina@172.21.57.30>
2019-03-11 13:52:43 +0100

    Aggiunti metodi standard

--- src/it/polito/tdp/libretto/Voti.java
+++ src/it/polito/tdp/libretto/Voti.java
@@ -1,5 +1,37 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+
 public class Voti {
 
+	private int voto;
+	private String corso;
+	private LocalDate data;
+	public Voti(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	
+	
 }
